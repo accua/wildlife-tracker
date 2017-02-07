ca7f14c17f3cb4cde2caf90f9a87b58ff872cf06
Andrew Accuardi and Ezra Sandzer-Bell <andrew+ezra@>
2017-02-06 16:57:10 -0800

    being creating a one-to-many for sightings

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,4 +1,5 @@
 class Sighting < ActiveRecord::Base
+  belongs_to :animal
   validates :date, :presence => true
   validates :latitude, :presence => true
   validates :longitude, :presence => true
---
+++ db/migrate/20170207005109_add_foreign_key_to_sighting.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyToSighting < ActiveRecord::Migration[5.0]
+  def change
+    add_column :sightings, :animal_id, :integer
+  end
+end
